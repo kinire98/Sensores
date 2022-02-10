b02e22d0723a6622aac0cad944fc7e0b966ef87e
kinire98 <iknitgarri@outlook.com>
2022-02-10 14:20:07 +0100

    Mecanismo de movimiento del punto para poder medir su posición

--- FrontEnd/escuchaDeEventos.js
+++ FrontEnd/escuchaDeEventos.js
@@ -1,14 +1,32 @@
-addEventListener('keypress', () => {
+let comprobarDistancia = false
+setInterval(() => {addEventListener('keydown', () => {
     let tecla = event.key;
-    let codigo = event.code;
-
-    if (tecla == 'w') {
-
-    } else if (tecla == 'a') {
-
-    } else if (tecla == 's') {
-
-    } else if (tecla == 'd')
-    console.log(`Código ${codigo}`)
-    console.log(`Tecla ${tecla}`)
-})
+    let arriba = parseFloat(document.getElementById('divInterno').style.top);
+    let izquierda = parseFloat(document.getElementById('divInterno').style.left);
+    let anchura = parseInt(document.getElementById('width').value);
+    let altura = parseInt(document.getElementById('height').value);
+    if (tecla == 'w' && !comprobarDistancia) {
+        if (arriba >= 0) {
+            document.getElementById('divInterno').style.top = arriba - 1 +'px'
+        }
+    }  
+    if (tecla == 'a'  && !comprobarDistancia) {
+        if (izquierda >= 0) {
+            document.getElementById('divInterno').style.left = izquierda - 1 +'px'
+        }
+    }  
+    if (tecla == 's'  && !comprobarDistancia) {
+        if (arriba <= altura - 6) {
+            document.getElementById('divInterno').style.top = arriba + 1 +'px'
+        }    
+        console.log(arriba)
+    }  
+    if (tecla == 'd'  && !comprobarDistancia) {
+        if (izquierda <= anchura - 6) {
+            document.getElementById('divInterno').style.left = izquierda + 1 +'px';
+        }
+    }
+    if (tecla == 'Enter' && !comprobarDistancia) {
+        comprobarDistancia = true
+    }
+})},500)
