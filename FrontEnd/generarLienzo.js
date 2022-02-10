d5854dcaa0d9aa5f2b8d06ee157aefa729a8b789
kinire98 <iknitgarri@outlook.com>
2022-02-10 09:56:15 +0100

    Añadí para que cúando generas el lienzo desaparezca las entradas para la generación del lienzo

--- FrontEnd/generarLienzo.js
+++ FrontEnd/generarLienzo.js
@@ -4,5 +4,6 @@ const generarLienzo = () => {
     document.getElementById('lienzo').style.width = anchura + 'px'
     document.getElementById('lienzo').style.height = altura +'px'
     document.getElementById('lienzo').style.border = '5px solid black'
+    document.getElementById('inferior').style.display = 'none'
 }
 
