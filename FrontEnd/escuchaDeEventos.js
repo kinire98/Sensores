3d64595fd20fe18cab14ad578b85ba92c2ea0695
kinire98 <iknitgarri@outlook.com>
2022-02-11 10:02:42 +0100

    Quité el setInterval, daba errores

--- FrontEnd/escuchaDeEventos.js
+++ FrontEnd/escuchaDeEventos.js
@@ -1,6 +1,6 @@
 let comprobarDistancia = false;
 let cadenaDeTxtBDD = '';
-setInterval(() => {addEventListener('keypress', () => {
+addEventListener('keypress', () => {
     let tecla = event.key;
     let arriba = parseFloat(document.getElementById('divInterno').style.top);
     let izquierda = parseFloat(document.getElementById('divInterno').style.left);
@@ -8,30 +8,30 @@ setInterval(() => {addEventListener('keypress', () => {
     let altura = parseInt(document.getElementById('height').value);
     if (tecla == 'w' && !comprobarDistancia) {
         if (arriba >= 0) {
-            document.getElementById('divInterno').style.top = arriba - 0.5 +'px';
+            document.getElementById('divInterno').style.top = arriba - 10 +'px';
             cadenaDeTxtBDD += 'w';
         }
     }  
     if (tecla == 'a'  && !comprobarDistancia) {
         if (izquierda >= 0) {
-            document.getElementById('divInterno').style.left = izquierda - 0.5  +'px';
+            document.getElementById('divInterno').style.left = izquierda - 10  +'px';
             cadenaDeTxtBDD += 'a';
         }
     }  
     if (tecla == 's'  && !comprobarDistancia) {
         if (arriba <= altura - 6) {
-            document.getElementById('divInterno').style.top = arriba + 0.5 +'px';
+            document.getElementById('divInterno').style.top = arriba + 10 +'px';
             cadenaDeTxtBDD += 's';
         }    
-        console.log(arriba)
     }  
     if (tecla == 'd'  && !comprobarDistancia) {
         if (izquierda <= anchura - 6) {
-            document.getElementById('divInterno').style.left = izquierda + 0.5  +'px';
+            document.getElementById('divInterno').style.left = izquierda + 10  +'px';
             cadenaDeTxtBDD += 'd';
         }
     }
     if (tecla == 'Enter' && !comprobarDistancia) {
         comprobarDistancia = true
     }
-})},500)
+    console.log(tecla)
+})
